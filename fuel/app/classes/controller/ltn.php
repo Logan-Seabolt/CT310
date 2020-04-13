221f0eb5f90b494e7a9c9e426c719359ced3bc8b
Logan-Seabolt <Seaboltl@ram.colostate.edu>
2020-04-13 17:21:13 -0600

    Lots of minor fixes and edits

---
+++ fuel/app/classes/controller/ltn.php
@@ -0,0 +1,21 @@
+<?php
+ 
+class Controller_ltn extends Controller_Template
+{
+
+	public function action_index()
+	{
+		$data = array();
+		$this->template->title = 'Lets Talk Numbers';
+		$this->template->content = View::forge('LetsTalkNumbers/index.php', $data);
+	}
+	public function action_about()
+	{
+		$data = array();
+		$this->template->title = 'About';
+		$this->template->content = View::forge('LetsTalkNumbers/about.php', $data);
+	}
+	
+}
+
+?>
---
+++ fuel/app/views/LetsTalkNumbers/about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head></head>
+	<body>
+
+	<div class="card">
+	<?php echo Asset::img('LS.jpg');?>
+	
+		<p>My undergraduate education was completed at Colorado State University Fort Collins. 
+		My bachelors degree is in Computer Science with a specialization on Human Centered Computing.
+		Human Centered Computing is a specialization of standard Computer Science that focuses 
+		on Psychology and how people interact with technology. I have applied to the Master's degree 
+		program for Computer Science at CSU Fort Collins with an interest researching the medical and 
+		social applications of Machine Learning and Artificial Intelligence.</p>
+        <p>I have many a quaint hobby that I partake in, including:<br>
+            <ul>
+                <li>Being a game master for Pathfinder and Dungeons and Dragons</li>
+                <li>Painting miniatures and figures for roleplaying games and tabletop wargames</li>
+                <li>Fencing and other combat styles with the <a href="https://www.sca.org/"> Society for Creative Anachronism</a></li>
+                <li>Playing tabletop wargames including Warhammer Fantasy 8th edition, Warhammer 40k, Warmachine, and Frostgrave</li>
+                <li>Playing and designing Video Games</li>
+            </ul>
+            </p>
+	</div>
+	<div class="card">
+	<?php echo Asset::img('TD.jpg');?>
+	
+		<p>I am in my final semester at CSU as a Computer Science Major. In addition,
+		I have earned a minor in Mathematics. I graduated high school in 2017, thus meaning 
+		I am graduating a year early. I took many AP classes and dual-enrollment,
+		so I came into CSU with many credits. I look forward to graduating in Spring 2020!</p>
+		<p>I first started programming in my Junior year of high school.
+		Although my teacher was less-than-impressive, I really grew to love programming. 
+		I decided to study computer science in college. In college my love for programming has flourished. 
+		My favorite languages are Java, C++, and Python. After college,
+		I plan on going into the industry as a software engineer. </p>
+	</div>
+	<div class="card">
+	<?php echo Asset::img('NS.jpg');?>
+	 <p>I am a Colorado State senior in Computer Science, graduating in the Spring 2020 with honors. 
+	 I am also president of Gamma Beta Phi, a scholar and service based organization. 
+	 I am planning to attend medical school after finishing my CS BS. I work as a Physics tutor on campus. 
+	 I am also an author of four novels, one of which was my senior honors thesis. </p>
+	 
+			  
+	<a href="/~nlstan/ct310/fuelviews/index.php/ltn/index">Nicole's Version</a>
+	</div>
+	</body>
+</html>
