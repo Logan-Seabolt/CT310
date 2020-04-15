edbe4913f90588a85452a476c3faad1c96a34373
tessdemuth <tessdemuth@users.noreply.github.com>
2020-04-15 17:55:14 -0600

    Adding hospital page to controller

--- fuel/app/classes/controller/ltn.php
+++ fuel/app/classes/controller/ltn.php
@@ -7,13 +7,19 @@ class Controller_ltn extends Controller_Template
 	{
 		$data = array();
 		$this->template->title = 'Lets Talk Numbers';
-		$this->template->content = View::forge('LetsTalkNumbers/index.php', $data);
+		$this->template->content = View::forge('LetsTalkNumbers/index', $data);
 	}
 	public function action_about()
 	{
 		$data = array();
 		$this->template->title = 'About';
-		$this->template->content = View::forge('LetsTalkNumbers/about.php', $data);
+		$this->template->content = View::forge('LetsTalkNumbers/about', $data);
+	}
+	public function action_hospitals()
+	{
+		$data = array();
+		$this->template->title = 'Hospitals';
+		$this->template->content = View::forge('LetsTalkNumbers/hospitals', $data);
 	}
 	
 }
