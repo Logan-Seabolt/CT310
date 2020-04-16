1dae88275905bb08a21317ecb3f1b4ae35d03df8
Nicole Standiford <nlstan@rams.colostate.edu>
2020-04-16 11:35:30 -0500

    Add files via upload

--- fuel/app/classes/controller/ltn.php
+++ fuel/app/classes/controller/ltn.php
@@ -18,13 +18,25 @@ class Controller_ltn extends Controller_Template
 	public function action_hospitals()
 	{
 		$data = array('hospitals' => LTNTables::get_table('hospitals'));
-		$this->template->title = 'Data Analysis';
+		$this->template->title = 'Hospital List';
 		$this->template->content = View::forge('LetsTalkNumbers/hospitals.php', $data);
 	}
 	public function action_drglist()
 	{
 		$data = array('drg_description' => LTNTables::get_table('drg_description'));
 		$this->template->title = 'DRG List';
+		$this->template->content = View::forge('LetsTalkNumbers/drglist.php', $data);
+	}
+	public function action_hospitaldetails()
+	{
+		$data = array('hospitals' => LTNTables::get_table('hospitals'));
+		$this->template->title = 'Hospital Details';
+		$this->template->content = View::forge('LetsTalkNumbers/hospitals.php', $data);
+	}
+	public function action_drgdetails()
+	{
+		$data = array('drg_description' => LTNTables::get_table('drg_description'));
+		$this->template->title = 'DRG Detaila';
 		$this->template->content = View::forge('LetsTalkNumbers/drglist.php', $data);
 	}
 }
