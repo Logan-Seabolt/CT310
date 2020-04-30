23b3f73976c39f02857b6aa5f810028db7236a69
tessdemuth <tessdemuth@users.noreply.github.com>
2020-04-30 17:25:32 -0600

    Pagination
    Pagination added to drg_description, drglist, and hospitals

--- fuel/app/views/LetsTalkNumbers/hospitals.php
+++ fuel/app/views/LetsTalkNumbers/hospitals.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php echo Asset::js("jquery.tablesorter.min.js"); ?>
+    <?php echo Asset::js("jquery.tablesorter.min.js"); 
+	if(isset($_GET['page'])){
+		if($_GET['page'] < 0){
+			$_GET['page'] = 0;
+		}
+		if($_GET['page'] >= ceil(count($hospitals)/ 20)){
+			$_GET['page'] = ceil(count($hospitals)/ 20)-1;
+		}
+	}
+	else{
+		$_GET['page'] = 0;
+	}
+	?>
     <script>
         $("document").ready(() => {
             $(function(){
@@ -25,16 +37,24 @@
         </thead>
         <tbody>
         <?php
-        foreach ($hospitals as $line) {
-            $id = $line['providerID'];
-            $name = $line['providerName'];
-            $state = $line['state'];
+		$start = 20 * $_GET['page'];
+		$end = min($start+20,count($hospitals));	
+		for($i = $start; $i < $end; $i++){
+			$id = $hospitals[$i]['providerID'];
+            $name = $hospitals[$i]['providerName'];
+            $state = $hospitals[$i]['state'];
             echo "<tr><td id='$id' class='Hname'>$name</td><td>$state</td><td>$id</td></tr>";
-        }
+		}
         ?>
         </tbody>
     </table>
 </div>
+<div id="previous">Previous</div>
+<form method="GET">
+    <span>Enter Page Number:</span><input type="text" name="page">
+    <input type="submit" value="Search Page">
+</form>
+<div id="next">Next</div>
 <script>
     <?php
     $temp = $GLOBALS["_SERVER"];
@@ -50,6 +70,22 @@
     $(".Hname").click(function() {
         window.open("/~" + "<?php echo $author?>" +"/ct310/LTN/index.php/ltn/hospitalDetails?id="+$(this).attr("id"));
     });
+	$("#previous").click(function (){
+		//var page = <?php echo($_GET['page']); ?>;
+		if(<?php echo($_GET['page']); ?> == 0){			
+		}
+		else{
+			window.open("/~" + "<?php echo $author?>" +"/ct310/LTN/index.php/ltn/hospitals?page="+<?php echo ($_GET['page']-1);?>, "_self");
+		}
+	});
+	$("#next").click(function (){
+		//var page = <?php echo($_GET['page']); ?>;
+		if(<?php echo($_GET['page']); ?> >= <?php echo (ceil(count($hospitals)/20));?>){		
+		}
+		else{
+			window.open("/~" + "<?php echo $author?>" +"/ct310/LTN/index.php/ltn/hospitals?page="+<?php echo ($_GET['page']+1);?>, "_self");
+		}
+	});
 </script>
 </body>
 </html>
